0d588bbcc61f01123e00c43d6a055f2b54e5b5ea
Roman Flammer <phalphalak@gmail.com>
2014-01-04 22:15:19 -0500

    Initial commit.

---
+++ test/starflight_code_wheel/core_test.clj
@@ -0,0 +1,7 @@
+(ns starflight-code-wheel.core-test
+  (:require [clojure.test :refer :all]
+            [starflight-code-wheel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
